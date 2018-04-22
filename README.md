81dcb7fcce6223205d9a74dc72c79ba136b93d17
Chang <584911924@qq.com>
2018-04-22 20:46:37 +0800

    初始化文件跟server

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # JSDataStructures
-学习JavaScript数据结构与算法
+
+学习 JavaScript 数据结构与算法
+
+敲码使我平静~~~
---
+++ mineType.js
@@ -0,0 +1,14 @@
+const type = {
+  '.': 'application/x-',
+  '.tif': 'image/tiff',
+  '.js': 'application/x-javascript',
+  '.json': 'application/json',
+  '.css': 'text/css',
+  '.jpg': 'image/jpeg',
+  '.png': 'image/png',
+  '.gif': 'image/gif',
+  '.html': 'text/html',
+  '.htm': 'text/html',
+};
+
+module.exports = type;
